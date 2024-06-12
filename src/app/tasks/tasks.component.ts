2fb10d20c56a61502b64edbc27d260d1af623e7b
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-06-12 16:12:46 +0300

    new-task component renders on click

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
     selector: 'app-tasks',
     standalone: true,
     templateUrl: './tasks.component.html',
     styleUrl: './tasks.component.css',
-    imports: [TaskComponent]
+    imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({required: true}) userId!: string;
   @Input({required: true}) name!: string;
+  isAddingTask = false;
   tasks = [
     {
       id: 't1',
@@ -48,6 +50,7 @@ export class TasksComponent {
   }
 
   onStartAddTask() {
+    this.isAddingTask = true;
     return console.log('onStartAddTask() is executed');
     
   }
