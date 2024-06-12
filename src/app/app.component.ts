f1166f49eedfab7cedccd27cfc996caaac4f1701
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-06-12 15:26:49 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, UserComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
 })
-
 export class AppComponent {
-  title = 'first-demo-project';
+  users = DUMMY_USERS;
+  // when default user is not specified, element <app-tasks/> component shouldn't be rendered
+  selectedUserId?: string;
+  // ---
+  // when default user is specified
+  // selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+    console.log('Selected user with id:' + id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+// computed and input import needed to use signals
+// import { Component, Input, Output, computed, input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  input,
+  output,
+} from '@angular/core';
+
+import { User } from './user.model'; 
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-export class UserComponent {}
+export class UserComponent {
+  // convincing ts with ! that we will receive values for avatar and name
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  // possible to set alternative Input property
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  // ---
+  // select = output<string>(); signal related approach;
+  // ---
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  // instead we're using with computed value (signal approach)
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+    // console.log("Clicked!");
+  }
+}
