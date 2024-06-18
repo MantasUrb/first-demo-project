4a0e94d33bea9d761a640b29ebff34d1c089780f
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-06-18 10:02:55 +0300

    standalone & module based components combined

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app/app.component";
+import { AppComponent } from './app/app.component';
+import { HeaderComponent } from './app/header/header.component';
+import { TasksComponent } from './app/tasks/tasks.component';
+import { UserComponent } from './app/user/user.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
 })
-
 export class AppModule {}
