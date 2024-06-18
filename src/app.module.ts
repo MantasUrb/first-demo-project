6b3f9cd6ed461539cb5a65816b2ec12021a348e9
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-06-18 09:50:18 +0300

    angular modules; intro

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app/app.component";
+
+@NgModule({
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
+// import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from './header/header.component';
+// import { UserComponent } from './user/user.component';
+// import { TasksComponent } from './tasks/tasks.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  // imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  // removing imports above since imports are not needed with modules
 })
 export class AppComponent {
   users = DUMMY_USERS;
